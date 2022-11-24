5c84a40d32e68356864fe64e24f9a9fd1e22c341
Thanchanokjj <118957249+Thanchanokjj@users.noreply.github.com>
2022-11-24 23:54:57 +0700

    Add files via upload

---
+++ 99/mdt211_2565_assignment3-main/mdt211_2565_assignment3-main/Program.cs
@@ -0,0 +1,57 @@
+﻿
+       class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<char> Input = new Queue<char>();
+        int n = 0;
+        while(n == 0){
+           char input = char.Parse(Console.ReadLine());     
+           if(input == 'L' || input == 'S' || input == 'M' ){
+               Input.Push(input);
+               n = 0;
+           } 
+           else{
+            n = 1;
+           }
+           n = n;; 
+        }
+        CircularLinkedList<int> output  = new CircularLinkedList<int>();
+      
+        while(true){
+            char T = Input.Pop();
+           if(T == 'L'){
+            Console.Write("1");
+            Input.Push('M');
+            Input.Push('M');
+            output.Add(1);
+           }
+           else if(T == 'M'){
+            Console.Write("2");
+            Input.Push('S');
+            Input.Push('S');
+            Input.Push('S');
+            output.Add(2);
+           }
+           else if(T == 'S'){
+            Console.Write("3");
+            output.Add(3);
+           }
+           if(Input.GetLength() == 0){
+            break;
+           }
+
+        }
+        int N = 0;
+        while(true){
+            if(N >= Input.GetLength()){
+                break;
+            }
+            Console.WriteLine(Input.Get(N));
+            N++;
+        }
+        
+
+    }
+}
+    
